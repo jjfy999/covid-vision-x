689bb2eb983994b69c3dea22aff62b3ad995a13c
emily <emilygong93@gmail.com>
2024-03-21 12:08:27 +0800

    update sa

---
+++ SystemAdminUI/noSearchBar.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("profiledropdown");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
