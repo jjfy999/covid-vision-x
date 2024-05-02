5e1bbb770a3a9f4f17a7b9f28405c44babfa3346
jjfy999 <jjfy999@uowmail.edu.au>
2024-05-03 00:29:14 +0800

    Change so logout routes to logoutUse func

--- frontend/src/pages/templates/DrawerAppBar.tsx
+++ frontend/src/pages/templates/DrawerAppBar.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Fade from '@mui/material/Fade';
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Fade from "@mui/material/Fade";
+import { useAuth } from "./AuthContexr";
 
 /* interface Props {
     window?: () => Window;
@@ -17,100 +18,117 @@ export default function DrawerAppBar(props: Props)
 */
 
 export default function DrawerAppBar() {
+  const [patientMenuAnchorEl, setPatientMenuAnchorEl] =
+    React.useState<null | HTMLElement>(null);
+  const openPatient = Boolean(patientMenuAnchorEl);
+  const handlePatientClick = (event: React.MouseEvent<HTMLElement>) => {
+    setPatientMenuAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setPatientMenuAnchorEl(null);
+    setProfileMenuAnchorEl(null);
+  };
 
-    const [patientMenuAnchorEl, setPatientMenuAnchorEl] = React.useState<null | HTMLElement>(null);
-    const openPatient = Boolean(patientMenuAnchorEl);
-    const handlePatientClick = (event: React.MouseEvent<HTMLElement>) => {
-        setPatientMenuAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setPatientMenuAnchorEl(null);
-        setProfileMenuAnchorEl(null);
-    };
+  const [profileMenuAnchorEl, setProfileMenuAnchorEl] =
+    React.useState<null | HTMLElement>(null);
+  const openProfile = Boolean(profileMenuAnchorEl);
+  const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
+    setProfileMenuAnchorEl(event.currentTarget);
+  };
+  const { logoutUser } = useAuth();
 
-    const [profileMenuAnchorEl, setProfileMenuAnchorEl] = React.useState<null | HTMLElement>(null);
-    const openProfile = Boolean(profileMenuAnchorEl);
-    const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
-        setProfileMenuAnchorEl(event.currentTarget);
-    };
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav" sx={{ bgcolor: "#267b83", padding: "8px 0" }}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            COVID VISION X
+          </Typography>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Button
+              sx={{
+                color: "#fff",
+                backgroundColor: "#ecbb50d7",
+                fontSize: "1rem",
+                padding: "6px 12px",
+                "&:hover": { backgroundColor: "#b2a434" },
+              }}
+              id="patient-button"
+              aria-controls={openPatient ? "fade-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={openPatient ? "true" : undefined}
+              onClick={handlePatientClick}
+            >
+              Patient
+            </Button>
+            <Menu
+              id="patient-menu"
+              MenuListProps={{
+                "aria-labelledby": "fade-button",
+              }}
+              anchorEl={patientMenuAnchorEl}
+              open={Boolean(patientMenuAnchorEl)}
+              onClose={handleClose}
+              TransitionComponent={Fade}
+              sx={{ mt: "45px" }}
+            >
+              <MenuItem
+                onClick={() => window.open("https://www.google.com", "_blank")}
+              >
+                Upload X-Ray Image
+              </MenuItem>
+              <MenuItem
+                onClick={() => window.open("https://www.fb.com", "_blank")}
+              >
+                Report
+              </MenuItem>
+              <MenuItem
+                onClick={() => window.open("https://www.twitter.com", "_blank")}
+              >
+                Non-Uploaded Report
+              </MenuItem>
+            </Menu>
 
+            <div style={{ width: "100px" }}></div>
 
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar component="nav" sx={{ bgcolor: '#267b83', padding: '8px 0' }}>
-                <Toolbar sx={{ justifyContent: 'space-between' }}>
-                    <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-                        COVID VISION X
-                    </Typography>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Button
-                            sx={{
-                                color: '#fff',
-                                backgroundColor: '#ecbb50d7',
-                                fontSize: '1rem',
-                                padding: '6px 12px',
-                                '&:hover': { backgroundColor: '#b2a434' },
-                            }}
-                            id="patient-button"
-                            aria-controls={openPatient ? 'fade-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={openPatient ? 'true' : undefined}
-                            onClick={handlePatientClick}
-                        >
-                            Patient
-                        </Button>
-                        <Menu
-                            id="patient-menu"
-                            MenuListProps={{
-                                'aria-labelledby': 'fade-button',
-                            }}
-                            anchorEl={patientMenuAnchorEl}
-                            open={Boolean(patientMenuAnchorEl)}
-                            onClose={handleClose}
-                            TransitionComponent={Fade}
-                            sx={{ mt: '45px' }}
-                        >
-                            <MenuItem onClick={() => window.open('https://www.google.com', '_blank')}>Upload X-Ray Image</MenuItem>
-                            <MenuItem onClick={() => window.open('https://www.fb.com', '_blank')}>Report</MenuItem>
-                            <MenuItem onClick={() => window.open('https://www.twitter.com', '_blank')}>Non-Uploaded Report</MenuItem>
-                        </Menu>
-
-                        <div style={{ width: '100px' }}></div>
-
-                        <Button
-                            sx={{
-                                color: '#fff',
-                                backgroundColor: '#ecbb50d7',
-                                fontSize: '1rem',
-                                padding: '6px 12px',
-                                '&:hover': { backgroundColor: '#b2a434' },
-                            }}
-                            id="profile-button"
-                            aria-controls={openProfile ? 'fade-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={openProfile ? 'true' : undefined}
-                            onClick={handleProfileClick}
-                        >
-                            Profile
-                        </Button>
-                        <Menu
-                            id="profile-menu"
-                            MenuListProps={{
-                                'aria-labelledby': 'fade-button',
-                            }}
-                            anchorEl={profileMenuAnchorEl}
-                            open={Boolean(profileMenuAnchorEl)}
-                            onClose={handleClose}
-                            TransitionComponent={Fade}
-                            sx={{ mt: '45px' }}
-                        >
-                            <MenuItem onClick={() => window.open('https://www.yahoo.com', '_blank')}>View Profile</MenuItem>
-                            <MenuItem onClick={() => window.open('https://www.apple.com', '_blank')}>Logout</MenuItem>
-                        </Menu>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+            <Button
+              sx={{
+                color: "#fff",
+                backgroundColor: "#ecbb50d7",
+                fontSize: "1rem",
+                padding: "6px 12px",
+                "&:hover": { backgroundColor: "#b2a434" },
+              }}
+              id="profile-button"
+              aria-controls={openProfile ? "fade-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={openProfile ? "true" : undefined}
+              onClick={handleProfileClick}
+            >
+              Profile
+            </Button>
+            <Menu
+              id="profile-menu"
+              MenuListProps={{
+                "aria-labelledby": "fade-button",
+              }}
+              anchorEl={profileMenuAnchorEl}
+              open={Boolean(profileMenuAnchorEl)}
+              onClose={handleClose}
+              TransitionComponent={Fade}
+              sx={{ mt: "45px" }}
+            >
+              <MenuItem
+                onClick={() => window.open("https://www.yahoo.com", "_blank")}
+              >
+                View Profile
+              </MenuItem>
+              <MenuItem onClick={logoutUser}>Logout</MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
