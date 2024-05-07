b7e440b887a8789a06bcd8df6096f449ce8f6477
emily <emilygong93@gmail.com>
2024-05-07 18:54:36 +0800

    update global ui

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../theme/login.css";
+import "../../../static/login/login.css";
 import logo from "../../../static/images/logo.png";
 import { useAuth } from "./templates/AuthContexr";
 import DrawerAppBar from "./templates/DrawerAppBar";
@@ -81,135 +81,135 @@ function LoginPage() {
   };
 
   return (
-    // <body className="loginPage">
-          <div id="container">
-      <div id="loginSection">
-        {isLoginView ? (
-          <div id="loginForm">
-            <h1 id="loginHeading">LOGIN</h1>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="role">
-                <b>Login As</b>
-              </label>
-              <select
-                value={role}
-                onChange={(e) => setRole(e.target.value)}
-                required
-                id="roleInput"
-              >
-                <option value="" disabled selected>
-                  Select a role
-                </option>
-                <option value="patient">Patient</option>
-                <option value="doctor">Doctor</option>
-                <option value="sysad">System Administrator</option>
-              </select>
+    <body className="loginPageContainer">
+      <div id="container">
+        <div id="loginSection">
+          {isLoginView ? (
+            <div id="loginForm">
+              <h1 id="loginHeading">LOGIN</h1>
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="role">
+                  <b>Login As</b>
+                </label>
+                <select
+                  value={role}
+                  onChange={(e) => setRole(e.target.value)}
+                  required
+                  id="roleInput"
+                >
+                  <option value="" disabled selected>
+                    Select a role
+                  </option>
+                  <option value="patient">Patient</option>
+                  <option value="doctor">Doctor</option>
+                  <option value="sysad">System Administrator</option>
+                </select>
 
-              <label htmlFor="uname">
-                <b>Username</b>
-              </label>
-              <input
-                type="text"
-                placeholder="Enter Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
+                <label htmlFor="uname">
+                  <b>Username</b>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
 
-              <label htmlFor="psw">
-                <b>Password</b>
-              </label>
-              <input
-                type="password"
-                placeholder="Enter Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+                <label htmlFor="psw">
+                  <b>Password</b>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
 
-              <button type="submit">Login</button>
+                <button type="submit">Login</button>
 
-              <a
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setIsLoginView(false);
-                }}
-                style={{
-                  color: "blue",
-                  cursor: "pointer",
-                  textDecoration: "underline",
-                }}
-              >
-                Don't have an account? Sign up
-              </a>
-            </form>
-          </div>
-        ) : (
-          <div>
-            <h1 id="loginHeading">SIGN UP</h1>
-            <form onSubmit={handleSignup}>
-              <label htmlFor="uname">
-                <b>Username</b>
-              </label>
-              <input
-                type="text"
-                placeholder="Enter Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setIsLoginView(false);
+                  }}
+                  style={{
+                    color: "blue",
+                    cursor: "pointer",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Don't have an account? Sign up
+                </a>
+              </form>
+            </div>
+          ) : (
+            <div>
+              <h1 id="loginHeading">SIGN UP</h1>
+              <form onSubmit={handleSignup}>
+                <label htmlFor="uname">
+                  <b>Username</b>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
 
-              <label htmlFor="psw">
-                <b>Password</b>
-              </label>
-              <input
-                type="password"
-                placeholder="Enter Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+                <label htmlFor="psw">
+                  <b>Password</b>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
 
-              <label htmlFor="confirmPsw">
-                <b>Confirm Password</b>
-              </label>
-              <input
-                type="password"
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
+                <label htmlFor="confirmPsw">
+                  <b>Confirm Password</b>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
 
-              <button type="submit">Sign Up</button>
+                <button type="submit">Sign Up</button>
 
-              <a
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-                  setIsLoginView(true);
-                }}
-                style={{
-                  color: "blue",
-                  cursor: "pointer",
-                  textDecoration: "underline",
-                }}
-              >
-                Already have an account? Log in
-              </a>
-            </form>
-          </div>
-        )}
-      </div>
-
-      {/* <div id="logoSection">
-        <div id="img-box">
-          <img src={logo} id="logoImg" alt="Company Logo" />
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setIsLoginView(true);
+                  }}
+                  style={{
+                    color: "blue",
+                    cursor: "pointer",
+                    textDecoration: "underline",
+                  }}
+                >
+                  Already have an account? Log in
+                </a>
+              </form>
+            </div>
+          )}
         </div>
-      </div> */}
-    </div>
-    // </body>
+
+        {/* <div id="logoSection">
+          <div id="img-box">
+            <img src={logo} id="logoImg" alt="Company Logo" />
+          </div>
+        </div> */}
+      </div>
+    </body>
 
   );
 }
