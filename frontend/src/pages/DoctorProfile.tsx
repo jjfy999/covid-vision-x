a8fdccb9e46b9404ce6f8bdf8936c8a354a3897a
jjfy999 <jjfy999@uowmail.edu.au>
2024-05-13 14:31:01 +0800

    fix phone_number

--- frontend/src/pages/DoctorProfile.tsx
+++ frontend/src/pages/DoctorProfile.tsx
@@ -1,23 +1,17 @@
 import ProfileCard from "./templates/ProfileCard";
-import UploadImage from "./templates/UploadImage";
-import NonUpdatedReport from "./templates/NonUpdatedReport";
 import Header from "./templates/Header";
-import DrawerAppBar from "./templates/DrawerAppBar";
 import { useEffect, useState } from "react";
 
 const App = () => {
-    console.log("DoctorProfile.tsx: App(), is being rendered.");
     const [docProfile, setDocProfile] = useState<any>(null);
 
     useEffect(() => {
         const fetchdata = async () => {
-            console.log("Inside fetchdata()");
             try {
                 const tokens = JSON.parse(
                     localStorage.getItem("authTokens") || "{}"
                 );
                 const token = tokens.access;
-                console.log("Token: ", token);
                 const res = await fetch("/baseUrl/docProfileView/", {
                     method: "GET",
                     headers: {
@@ -25,7 +19,6 @@ const App = () => {
                         Authorization: "Bearer " + token,
                     },
                 });
-                console.log("fetch Completed");
                 if (!res.ok) {
                     throw new Error("http error: status " + res.status);
                 }
@@ -64,61 +57,13 @@ const App = () => {
                             username={docProfile.username}
                             password={docProfile.password}
                             role={docProfile.role}
-                            contactNumber={docProfile.phone_number}
+                            phone_number={docProfile.phone_number}
                             email={docProfile.email}
                             pageContext="profile"
                         />
                     )}
                 </div>
             </div>
-            {/* 
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "calc(100vh - 450px)",
-                    marginTop: "300px",
-                }}
-            >
-                <div
-                    style={{
-                        flexShrink: 0,
-                        width: "100%",
-                        maxWidth: "600px",
-                    }}
-                >
-                    <UploadImage
-                        userRole="doctor"
-                        onFileUpload={(file, patientName) => {
-                            console.log(file, patientName);
-                        }}
-                    />
-
-                    <UploadImage
-                        userRole="researcher"
-                        onFileUpload={(file, patientName) => {
-                            console.log(file, patientName);
-                        }}
-                    />
-                </div>
-            </div>
-
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "100vh",
-                }}
-            >
-                <NonUpdatedReport
-                // patientName="John Tan"
-                // date="23 March 2024"
-                // imageUrl="path_to_xray_image.png"
-                // initialStatus="covid"
-                />
-            </div> */}
         </>
     );
 };
--- frontend/src/pages/templates/ProfileCard.tsx
+++ frontend/src/pages/templates/ProfileCard.tsx
@@ -16,7 +16,7 @@ interface ProfileProps {
     id: string;
     name: string;
     role: string;
-    contactNumber: string;
+    phone_number: string;
     email: string;
     username: string;
     password: string;
@@ -116,9 +116,6 @@ const ProfileCard: React.FC<ProfileProps> = (props) => {
                 <AccountCircleIcon sx={{ fontSize: 100, mt: 2 }} />
             </CardMedia>
             <CardContent>
-                {/* <Typography gutterBottom variant="h5" component="div" sx={{ marginLeft: '15px', fontWeight: 'bold' }}>
-                    Doctor Profile
-                </Typography> */}
                 <List>
                     {editMode ? (
                         <>
@@ -170,8 +167,8 @@ const ProfileCard: React.FC<ProfileProps> = (props) => {
                                     label="Contact Number"
                                     variant="outlined"
                                     fullWidth
-                                    value={profile.contactNumber}
-                                    onChange={handleChange("contactNumber")}
+                                    value={profile.phone_number}
+                                    onChange={handleChange("phone_number")}
                                 />
                             </ListItem>
                             <ListItem>
@@ -219,7 +216,7 @@ const ProfileCard: React.FC<ProfileProps> = (props) => {
                             <ListItem>
                                 <ListItemText
                                     primary="Contact Number:"
-                                    secondary={profile.contactNumber}
+                                    secondary={profile.phone_number}
                                 />
                             </ListItem>
                             <ListItem>
@@ -318,60 +315,3 @@ const ProfileCard: React.FC<ProfileProps> = (props) => {
 };
 
 export default ProfileCard;
-
-//old below
-
-// import React from 'react';
-// import { Card, CardContent, Typography, Button, CardMedia, List, ListItem, ListItemText } from '@mui/material';
-// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-
-// interface ProfileProps {
-//     doctorId: string;
-//     name: string;
-//     contactNumber: string;
-//     email: string;
-// }
-
-// const ProfileCard: React.FC<ProfileProps> = ({ doctorId, name, contactNumber, email }) => {
-//     return (
-//         <Card sx={{ maxWidth: 345, boxShadow: 3, borderRadius: 2, m: 2 }}>
-//             <CardMedia
-//                 sx={{
-//                     display: 'flex', // Enable flexbox
-//                     justifyContent: 'center', // Center horizontally
-//                     alignItems: 'center', // Center vertically (optional if you want it exactly in the middle of the space)
-//                     height: 140 // Set a specific height for the CardMedia container
-//                 }}
-//             >
-//                 <AccountCircleIcon sx={{ fontSize: 100, mt: 2 }} />
-//             </CardMedia>
-//             <CardContent>
-//                 {/* <Typography gutterBottom variant="h5" component="div">
-//                     Doctor Profile
-//                 </Typography> */}
-//                 <Typography gutterBottom variant="h5" component="div" sx={{ marginLeft: '15px', fontWeight: 'bold' }}>
-//                     Doctor Profile
-//                 </Typography>
-//                 <List>
-//                     <ListItem>
-//                         <ListItemText primary="Doctor ID:" secondary={doctorId} />
-//                     </ListItem>
-//                     <ListItem>
-//                         <ListItemText primary="Doctor Name:" secondary={name} />
-//                     </ListItem>
-//                     <ListItem>
-//                         <ListItemText primary="Contact Number:" secondary={contactNumber} />
-//                     </ListItem>
-//                     <ListItem>
-//                         <ListItemText primary="Email:" secondary={email} />
-//                     </ListItem>
-//                 </List>
-//                 <Button variant="contained" color="primary" sx={{ mt: 1 }}>
-//                     Edit
-//                 </Button>
-//             </CardContent>
-//         </Card>
-//     );
-// };
-
-// export default ProfileCard;
