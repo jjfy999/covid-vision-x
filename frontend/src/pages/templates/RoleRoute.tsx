2aea5a7eb1bb18e3e7f674e2ef056dff24b8d557
jjfy999 <jjfy999@uowmail.edu.au>
2024-05-15 04:35:18 +0800

    fixed error roleRoute

--- frontend/src/pages/templates/RoleRoute.tsx
+++ frontend/src/pages/templates/RoleRoute.tsx
@@ -1,15 +1,15 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContexr"; // Assuming AuthContext
 
-const RoleRoute = ({ requiredRole }) => {
-  const { user } = useAuth();
+const RoleRoute = ({ requiredRole }: { requiredRole: string[] }) => {
+    const { user } = useAuth();
 
-  if (!user || (user && !requiredRole.includes(user.role))) {
-    // Redirect if not logged in or role doesn't match
-    return <Navigate to="/loginpage" replace />;
-  }
+    if (!user || (user && !requiredRole.includes(user.role))) {
+        // Redirect if not logged in or role doesn't match
+        return <Navigate to="/loginpage" replace />;
+    }
 
-  return <Outlet />;
+    return <Outlet />;
 };
 
 export default RoleRoute;
