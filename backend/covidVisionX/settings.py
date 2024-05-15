fb660a8fd0eb70a95fa000f1396d5ac1681478fc
Bronson <bronson.chan@hotmail.com>
2024-05-16 00:33:04 +0800

    .

--- backend/covidVisionX/settings.py
+++ backend/covidVisionX/settings.py
@@ -253,7 +253,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 CORS_ALLOWED_ORIGINS = [
-    'https://covid-vision-x-three.vercel.app'
+    'https://covid-vision-x-three.vercel.app/'
 ]
 
 CORS_ALLOW_METHODS = [
