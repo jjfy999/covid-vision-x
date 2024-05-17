d6b3aea7007575bb46989092215a2f9996367abb
Bronson <bronson.chan@hotmail.com>
2024-05-17 21:52:56 +0800

    .

---
+++ backend/covidVisionX/settings_test.py
@@ -0,0 +1,15 @@
+from .settings import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
