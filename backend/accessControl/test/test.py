e689ec226e08aa69ef442fd2e73ef4ecff0e4546
jjfy999 <jjfy999@uowmail.edu.au>
2024-05-18 00:16:56 +0800

    accessControl test

--- backend/accessControl/test/test.py
+++ backend/accessControl/test/test.py
@@ -1,14 +1,14 @@
 from accessControl.views import MyTokenObtainPairSerializer
-from django.contrib.auth.models import User
 from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework_simplejwt.tokens import RefreshToken
+from userAccount.models import Account
 
 
 class MyTokenObtainPairSerializerTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser', password='testpass')
+        self.user = Account.objects.create_user(
+            username='testuser', password='testpass', email="test@test.com", phone_number="1234567890")
         self.user.role = 'admin'
         self.user.save()
 
@@ -16,13 +16,3 @@ class MyTokenObtainPairSerializerTest(TestCase):
         token = MyTokenObtainPairSerializer.get_token(self.user)
         self.assertEqual(token['username'], 'testuser')
         self.assertEqual(token['role'], 'admin')
-
-
-class GetRoutesTest(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-
-    def test_get_routes(self):
-        response = self.client.get('/api/routes')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, ['/api/token', '/api/token/refresh'])
